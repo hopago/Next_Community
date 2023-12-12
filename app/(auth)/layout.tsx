16a4e5f2ff1422ec5ddeaa83404ac63e3f2adf60
hopago <132269386+hopago@users.noreply.github.com>
2023-12-12 19:21:12 +0900

    layout centered

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>
-          <div className="w-full flex justify-center min-h-screen">
+          <div className="w-full flex items-center justify-center min-h-screen">
             {children}
           </div>
         </body>
